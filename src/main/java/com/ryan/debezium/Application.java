ad99e85743b447da17c19a7ff00d7204929016b6
Gogs <gogs@fake.local>
2022-07-17 21:55:39 +0800

    初始化1.0.5版本提交

---
+++ src/main/java/com/ryan/debezium/Application.java
@@ -0,0 +1,16 @@
+package com.ryan.debezium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ryan
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
